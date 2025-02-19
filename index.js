3a33a3be601756bd3e1e6e7f16b35f6baa62d056
vinceevogue <ferreiravincent115@gmail.com>
2025-02-19 14:09:04 +0100

    solve lab

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`;
 
 // Print out the concatenated string
-
+console.log(tongueTwister);
 
 
 
@@ -20,12 +20,13 @@ const s5 = "and";
 *******************************************/
 const part1 = "java";
 const part2 = "script";
+const result = part1.slice(0, part1.length - 1) + part1[part1.length - 1].toUpperCase() + part2.slice(0, part2.length - 1) + part2[part2.length - 1].toUpperCase();
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
 
 // Print the cameLtaiL-formatted string
-
+console.log(result)
 
 
 
@@ -35,10 +36,10 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+const tipAmount = billTotal * 15 /100
 
 // Print out the tipAmount
-
+console.log(tipAmount);
 
 
 
@@ -47,10 +48,9 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
-
+let randomNumber = Math.floor(Math.random() * 10) + 1
 // Print the generated random number
-
+console.log(randomNumber);
 
 
 /*******************************************
@@ -62,15 +62,30 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+// ! false
 
 const expression2 = a || b;
+// ! true
 
 const expression3 = !a && b;
+//! false
 
 const expression4 = !(a && b);
+//! true
 
 const expression5 = !a || !b;
+// !true
 
 const expression6 = !(a || b);
+// ! false
 
 const expression7 = a && a;
+// ! true
+
+console.log(expression1);
+console.log(expression2);
+console.log(expression3);
+console.log(expression4);
+console.log(expression5);
+console.log(expression6);
+console.log(expression7);
